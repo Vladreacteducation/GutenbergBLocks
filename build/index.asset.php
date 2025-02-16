c031209451f2392a16f1d947182bddb95c98e21e
Vladpopovich20 <vladyslavpopovich@gmail.com>
2025-02-16 20:30:02 +0200

    metadata

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'fea4a9f036e79436e2c3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '3cc3645e77431081db65');
--- dynamicblock.php
+++ dynamicblock.php
@@ -7,8 +7,8 @@
  * Author URI: #
  */
 
- function genius_myblock_init(){
+ function genius_dynamicblock_init(){
     register_block_type_from_metadata( __DIR__ );
  }
- add_action('init','genius_myblock_init');
+ add_action('init','genius_dynamicblock_init');
 
