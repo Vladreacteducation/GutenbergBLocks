f112ec2aebddd586f59d2bc965afaa73e9f01509
Vladpopovich20 <vladyslavpopovich@gmail.com>
2025-02-16 20:11:22 +0200

    first commit Dynamic block

---
+++ myblock.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Dynamic Posts
+ * Plugin URI: #
+ * Description: Dynamic Gutenberg Block
+ * Author: Vlad Leaf
+ * Author URI: #
+ */
+
+ function genius_myblock_init(){
+    register_block_type_from_metadata( __DIR__ );
+ }
+ add_action('init','genius_myblock_init');
+
